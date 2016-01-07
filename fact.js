befa76bf853f5acf2b07d7b28b3b96f64dd48b7e
Chris Rose <cdrose15@gmail.com>
2016-01-06 23:11:42 -0500

    Factorial
    1st upload

---
+++ fact.js
@@ -0,0 +1,18 @@
+﻿
+function getFactorial() {
+    var num = document.getElementById("num").value
+    document.getElementById("fac").innerHTML = "Calculation = " + factorial(num);
+}
+
+
+function factorial(num) {
+    if (num == 0) {
+        return 1;
+    }
+    if (num < 0) {
+        return "Please enter a positive number";
+    }
+    else {
+        return num * factorial(num - 1);
+    }
+}
