675be8a4b4def7d6c9b16eea72cda419b656ab99
Chris Rose <cdrose15@gmail.com>
2016-01-11 14:07:09 -0500

    1/11/15
    edits

--- fact.js
+++ fact.js
@@ -1,18 +1,35 @@
 ﻿
-function getFactorial() {
-    var num = document.getElementById("num").value
-    document.getElementById("fac").innerHTML = "Calculation = " + factorial(num);
-}
+//function getFactorial() {
+//    var num = document.getElementById("num").value
+//    document.getElementById("fac").innerHTML = "Calculation = " + factorial(num);
+//}
 
 
-function factorial(num) {
-    if (num == 0) {
-        return 1;
-    }
-    if (num < 0) {
-        return "Please enter a positive number";
-    }
-    else {
-        return num * factorial(num - 1);
-    }
-}
+//function factorial(num) {
+//    if (num == 0) {
+//        return 1;
+//    }
+//    if (num < 0) {
+//        return "Please enter a positive number";
+//    }
+//    else {
+//        return num * factorial(num - 1);
+//    }
+//}
+
+$(document).ready(function() {
+    $("#submit").click(function () {
+        
+            var x = $("#num").val();
+
+            if (x == 0) {
+                $("#fac").text('1');
+            }
+            else if (x < 0) {
+                $("#fac").text("Undefined - Please enter a positive number");
+            }
+            else {
+                $("#fac").text(x * arguments.callee(x - 1));
+            }
+    });
+});
